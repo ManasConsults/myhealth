ff8d06bea928e7d958a78099c1d1e860d39abf0d
Manas Mallick <manasm.dev@gmail.com>
2026-05-13 20:46:02 +1000

    updated prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +9,6 @@ export default defineConfig({
     seed: "node node_modules/tsx/dist/cli.mjs prisma/seed.ts",
   },
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL ?? "",
   },
 });
